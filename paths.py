92735685eca0ca05f755700fc0e1f7ee90360ed8
maxisi <isi.maxi@gmail.com>
2013-08-05 22:11:15 -0700

    fixed file structure

--- paths.py
+++ paths.py
@@ -1,8 +1,42 @@
 '''
 Contains all paths used by different modules, making it easy to make changes.
+
+File structure:
+files/
+    analysis/
+        results/
+    data/
+        ephemerides/
+    background/
+    templates/
+        antennapatterns/
+    remote/
+        source/
+        ephemerides/
+plots/
+        
 '''
+import os
 
 from numpy import pi
+
+leafList = ('analysis/results/', 'data/ephemerides/', 'background/', 'templates/antennapatterns')
+def makestructure():
+    print 'Creating file structure:'
+    # create files/
+    for dir in leafList:
+        try:
+            os.makedirs('files/' + dir)
+            print dir
+        except OSError:
+            print 'Error: files/%(dir)s already exists' % locals()
+    # create plots/
+    try:
+        os.mkdir('plots/')
+        print 'plots/'
+    except OSError:
+        print 'Error: plots/ already exists'
+
 
 # Antenna patterns
 psrlist = 'files/templates/psrlist.txt'         # list of pulsars
@@ -14,12 +48,12 @@ originalData = 'files/remote/source' # '../../matthew/analyses/S6_all/results'
 importedData = 'files/data/dataPitkin_'         # original data in DataFrame
 
 # Detector
-vectors = 'files/templates/vectors/'                    # detector and wave vectors
+vectors = 'files/templates/vectors/'            # detector and wave vectors
 
 # Source
 textfromATNF = 'files/templates/psrcat.txt'     # download from ATNF
 psrcat = 'files/templates/pulsar_catalogue'     # pulsar data in DF
-psrextra = 'files/templates/psrextra.txt'       # polarization and inclination angles
+psrextra = 'files/psrextra.txt'                 # polarization and inclination angles
 psrlist = 'files/templates/psrlist.txt'         # names of PSR names to analyze
 
 # Analysis
@@ -29,7 +63,7 @@ tmat = 'files/analysis/tmat'                    # basis transformation matrix
 
 # Ephemerides
 eph = 'files/remote/ephemerides/' #'usr/share/lalpulsar/'
-eph_local = 'files/ephemerides/'                # imported ephemerides in HDF5 format
+eph_local = 'files/data/ephemerides/'           # imported ephemerides in HDF5 format
 
 
 def pname(pinj):
