2eee1ae2c89ba17d080c2ec46029da1e751a5f03
maxisi <isi.maxi@gmail.com>
2013-08-13 22:14:49 -0700

    went back to 1 sigma
    the standard deviation is taken over real part, not amplitude of the
    signal. then both the real and imaginary parts of the data are divided
    by that number. in the future, should take Re and Im std separetely

--- process.py
+++ process.py
@@ -353,7 +353,7 @@ class InjSearch(object):
         self.t = self.background.seed.finehet.index
         
         sigma = Sigma(self.detector, self.psr, self.background.seed.finehet)
-        self.sg = np.sqrt(2) * sigma.std
+        self.sg = sigma.std
         
         # injection info
         inj = np.linspace(hinjrange[0], hinjrange[1], ninj)
