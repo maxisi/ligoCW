eda4172e74fb15378772458c7807f3e9cf92335a
maxisi <isi.maxi@gmail.com>
2013-08-15 17:24:16 -0700

    corrected MP10 super

--- process.py
+++ process.py
@@ -647,7 +647,7 @@ class ManyPulsars(object):
             f.close()
             
 class MP10(ManyPulsars):
-    def __init__(self, detector, n, methods=['GR', 'G4v', 'AP']):
-        super(self, MP10).__init__(detector, methods=methods)
+    def __init__(self, n, detector='H1', methods=['GR', 'G4v', 'AP']):
+        super(MP10, self).__init__(detector, methods=methods)
         self.analyze('GR', [n, 10], extra_name=str(n)+'-9')
     
